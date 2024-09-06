f7ec1ab86934846f3319e94e10aa302b1ce6ed28
Hachounet <hachounet.github@gmx.fr>
2024-09-06 13:57:42 +0200

    FEAT Drafts publish/unpublish

--- src/components/AllDrafts.jsx
+++ src/components/AllDrafts.jsx
@@ -1,10 +1,33 @@
+import { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import PostCard from './PostCard';
 import useAuth from '../hooks/authFetch';
-
 import CreatePostBtn from './CreatePostBtn';
+
 export default function AllDrafts({ url }) {
   const { data, loading, error } = useAuth(url);
+  const [drafts, setDrafts] = useState([]);
+
+  useEffect(() => {
+    if (data && data.drafts) {
+      setDrafts(data.drafts);
+      console.log(data);
+    }
+  }, [data]);
+
+  const handleDraftDelete = (draftId) => {
+    setDrafts((prevDrafts) =>
+      prevDrafts.filter((draft) => draft.id !== draftId)
+    );
+  };
+
+  const handleDraftPublish = (draftId) => {
+    setDrafts((prevDrafts) =>
+      prevDrafts.map((draft) =>
+        draft.id === draftId ? { ...draft, published: !draft.published } : draft
+      )
+    );
+  };
 
   if (loading) return <div>Loading drafts...</div>;
   if (error) return <div>An error occurred: {error.message}</div>;
@@ -16,13 +39,17 @@ export default function AllDrafts({ url }) {
       </div>
 
       <div className="grid grid-cols-3 grid-rows-4 gap-8 pt-8 pr-4 pl-4">
-        {data.drafts.map((draft) => (
+        {drafts.map((draft) => (
           <PostCard
             key={draft.id}
             author={draft.author.pseudo}
             createdAt={draft.createdAt}
             title={draft.title}
             content={draft.Content}
+            postId={draft.id}
+            published={draft.published}
+            onDelete={handleDraftDelete}
+            onPublish={handleDraftPublish}
           />
         ))}
       </div>
